d888f7552a72e77d98eefd830b7c58b2709fc1b2
Uddhav Narayan Bhat <bhatuddhav@gmail.com>
2024-09-05 13:42:00 +0530

    Form retrival

--- redact/app/views.py
+++ redact/app/views.py
@@ -4,4 +4,12 @@ from django.shortcuts import render
 
 
 def index(request):
+    if request.method == 'POST':
+         form_data = {
+            'files': request.FILES.getlist('files'),
+            'rangeInput': request.POST.get('rangeInput'),
+            'wordsTextarea': request.POST.get('wordsTextarea'),
+        }
+         print("Form Data Received:")
+         print(form_data)
     return render(request, 'index.html')

ad0ae27053fd5d5b675fc5a987c19cca2213cdc9
EeshaanBhat <79324365+SmartyDroidBot@users.noreply.github.com>
2024-09-21 15:27:05 +0530

    Not a README

--- redact/app/services/model_service.py
+++ redact/app/services/model_service.py
@@ -4,88 +4,18 @@ from PIL import Image, ImageDraw
 from .agents import config_list
 from .agents import TextRedactionAgents, ImageRedactionAgents, user_proxy
 from .guardrails import guardrail_proper_nouns, guardrail_proper_nouns_list, guardrail_capitalized_words, guardrail_phonesEmailsDates
-from .utils import azure_image_ocr, export_redacted_image
+from .utils import azure_image_ocr, export_redacted_image, redact_text
 from django.conf import settings
 
 class TextRedactionService:
-    def __init__(self, degree=0, guardrail_toggle=1):
-        self.degree = degree
+    def __init__(self, degree=1, guardrail_toggle=1):
+        self.degree = degree+1 # My degrees start from 1
         self.guardrail_toggle = guardrail_toggle
 
-        # Define the chat outline
-        self.chat_outline = {
-            'message': '',
-        }
-
-        self.text_assistant, self.evaluation = TextRedactionAgents(self.degree).text_assistant, TextRedactionAgents(self.degree).evaluation
-        self.user_proxy = user_proxy
-
     def redact_text(self, text):
-        global conversation_state
-        conversation_state = 1
-        speakers = []
-
-        def text_redact_selection_func(speaker: autogen.AssistantAgent, groupchat: autogen.GroupChat):
-            global conversation_state
-            if speaker == self.user_proxy and conversation_state == 1:
-                conversation_state += 1
-                speakers.append(self.text_assistant.name)
-                return self.text_assistant
-            elif speaker == self.text_assistant and conversation_state == 2:
-                conversation_state += 1
-                speakers.append(self.evaluation.name)
-                return self.evaluation
-            elif speaker == self.evaluation and conversation_state == 3:
-                conversation_state += 1
-                speakers.append(self.text_assistant.name)
-                return self.text_assistant
-
-        # Update chat outline with user-provided text
-        self.chat_outline['message'] = text
-
-        # Define the GroupChat and GroupChatManager
-        text_redaction_chat = autogen.GroupChat(
-            agents=[self.user_proxy, self.text_assistant, self.evaluation],
-            speaker_selection_method=text_redact_selection_func,
-            messages=[],
-            max_round=4
-        )
-
-        text_redaction_manager = autogen.GroupChatManager(
-            groupchat=text_redaction_chat, llm_config=config_list
-        )
-
-        # Initiate the chat
-        text_redaction_chats = self.user_proxy.initiate_chat(
-            text_redaction_manager,
-            message=self.chat_outline['message']
-        )
-
-        redacted_text_from_agent = text_redaction_chats.chat_history[-1]['content']
-        redacted_text = ''
-
+        redacted_text = redact_text(text, self.degree)
         # Return chat history
-        agent_speech = []
-        for i, chat in enumerate(speakers):
-            agent_speech.append('<h4>' + speakers[i] + '</h4>')
-            agent_speech.append('<p>' + text_redaction_chats.chat_history[i+1]['content'] + '</p>')
-        
-        # Guardrails are called only for last degree
-        if self.guardrail_toggle:        
-            redacted_text_no_proper_nouns = guardrail_proper_nouns(redacted_text_from_agent)
-            redacted_text_no_capitalized_words = guardrail_capitalized_words(redacted_text_no_proper_nouns)
-            redacted_text_no_phonesEmailsDates = guardrail_phonesEmailsDates(redacted_text_no_capitalized_words)
-            redacted_text = redacted_text_no_phonesEmailsDates
-
-            agent_speech.append('<h4>' + 'guardrail: redacting proper nouns' + '</h4>')
-            agent_speech.append('<p>' + redacted_text_no_proper_nouns + '</p>')
-            agent_speech.append('<h4>' + 'guardrail: redacting capitalized words' + '</h4>')
-            agent_speech.append('<p>' + redacted_text_no_capitalized_words + '</p>')
-            agent_speech.append('<h4>' + 'guardrail: redacting phones, emails, and dates' + '</h4>')
-            agent_speech.append('<p>' + redacted_text_no_phonesEmailsDates + '</p>')
-        else:
-            redacted_text = redacted_text_from_agent     
-
+        agent_speech = []   
         return redacted_text, agent_speech
 
 
--- redact/app/services/utils.py
+++ redact/app/services/utils.py
@@ -3,6 +3,7 @@ from azure.core.credentials import AzureKeyCredential
 from azure.ai.formrecognizer import DocumentAnalysisClient
 from django.conf import settings
 from PIL import Image, ImageDraw
+from transformers import pipeline, AutoTokenizer, AutoModelForTokenClassification
 
 # Azure OCR function for images
 def azure_image_ocr(image):
@@ -41,3 +42,95 @@ def export_redacted_image(image_path, redacted_cords):
     image.save(output_path)
 
     return output_path
+
+
+# Load the tokenizer and model from the local directory
+model_path = "D:\\Projects\\SIH\\RE-DACT\\redact\\app\\services\\deberta_finetuned_pii" # Fix this path thingy according to your conveniece
+tokenizer = AutoTokenizer.from_pretrained(model_path)
+model = AutoModelForTokenClassification.from_pretrained(model_path)
+
+# Create the pipeline with your local model
+gen = pipeline("token-classification", model=model, tokenizer=tokenizer, aggregation_strategy="first")
+
+def tag_model_output(model_output):
+    """
+    Returns:
+    list: A list of dictionaries with an additional 'tag' key indicating the level.
+    """
+    level_1 = {
+        "SSN", "CREDITCARDNUMBER", "CREDITCARDCVV", "PASSWORD", "IP", "MAC",
+        "BITCOINADDRESS", "ETHEREUMADDRESS", "LITECOINADDRESS", "ACCOUNTNUMBER",
+        "IBAN", "BIC"
+    }
+    
+    level_2 = {
+        "FIRSTNAME", "LASTNAME", "FULLNAME", "NAME", "EMAIL", "PHONE_NUMBER",
+        "STREETADDRESS", "CITY", "ZIPCODE", "STATE", "COUNTRY", "JOBTITLE",
+        "COMPANY_NAME", "USERNAME"
+    }
+    
+    level_3 = {
+        "PREFIX", "MIDDLENAME", "SUFFIX", "JOBDESCRIPTOR", "JOBAREA",
+        "SECONDARYADDRESS", "COUNTY", "CURRENCY", "CURRENCYSYMBOL",
+        "CURRENCYCODE", "USERAGENT", "SEX", "GENDER", "NEARBYGPSCOORDINATE",
+        "DISPLAYNAME", "SEXTYPE", "ORDINALDIRECTION"
+    }
+    
+    def tag_word(word):
+        if word in level_1:
+            return 1
+        elif word in level_2:
+            return 2
+        elif word in level_3:
+            return 3
+        else:
+            return 999  # Default to level 9 for all other items not classified
+
+    for entity in model_output:
+        entity['tag'] = tag_word(entity['entity_group'])
+    
+    return model_output
+
+def classify_and_tag_text(input_text):
+    # Returns a dictionary with words as keys and their corresponding tags as values.
+    classified_entities = gen(input_text)
+    tagged_entities = tag_model_output(classified_entities)
+    word_tag_dict = {entity['word'].strip(): entity['tag'] for entity in tagged_entities}
+    return word_tag_dict
+
+def redact_text(input_text, level):
+    # Idk how the multi word entries works, its all chatgpt.
+    
+    # Classify and tag the text
+    word_tag_mapping = classify_and_tag_text(input_text)
+    
+    # Initialize variables
+    words = input_text.split()
+    redacted_words = []
+    i = 0
+    
+    # Iterate through the words
+    while i < len(words):
+        word = words[i]
+        # Check for multi-word entities
+        for entity, tag in word_tag_mapping.items():
+            entity_words = entity.split()
+            if words[i:i+len(entity_words)] == entity_words:
+                if tag <= level:
+                    redacted_words.append('■■■■■' * len(entity_words))
+                else:
+                    redacted_words.extend(entity_words)
+                i += len(entity_words) - 1
+                break
+        else:
+            # If no multi-word entity is found, process the single word
+            if word_tag_mapping.get(word.strip(), 4) <= level:
+                redacted_words.append('■■■■■')
+            else:
+                redacted_words.append(word)
+        i += 1
+    
+    # Join the redacted words back into a single string
+    redacted_text = ' '.join(redacted_words)
+    
+    return redacted_text

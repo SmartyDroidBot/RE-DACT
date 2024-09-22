b67daeeb2932f326c29ba75d10b29f94a924b52a
Shashwat Saini <shashwatsaini290@gmail.com>
2024-09-22 13:43:50 +0530

    Feat: Added PDF Redaction (Limit: 2 pages)

--- redact/app/services/model_service.py
+++ redact/app/services/model_service.py
@@ -1,9 +1,9 @@
 import ast, os, string
 from PIL import Image, ImageDraw
 from .agents import config_list
-from .agents import TextRedactionAgents, ImageRedactionAgents, user_proxy
+from .agents import TextRedactionAgents, ImageRedactionAgents, PDFRedactionAgents, user_proxy
 from .guardrails import guardrail_proper_nouns, guardrail_emails
-from .utils import azure_image_ocr, export_redacted_image
+from .utils import azure_image_ocr, azure_pdf_ocr, export_redacted_image, export_redacted_pdf
 from django.conf import settings
 
 class TextRedactionService:
@@ -112,16 +112,87 @@ class ImageRedactionService:
         # Extract redacted words and their coordinates
         redacted_cords = []
         for page in result.pages:
-                for word in page.words:
-                    for redacted_word in redacted_list:
-                        if redacted_word in word.content:
-                            cords = []
-                            for polygon in word.polygon:
-                                cords.append((polygon.x, polygon.y))
-                            redacted_cords.append(cords)
+            for word in page.words:
+                for redacted_word in redacted_list:
+                    if redacted_word in word.content:
+                        cords = []
+                        for polygon in word.polygon:
+                            cords.append((polygon.x, polygon.y))
+                        redacted_cords.append(cords)
 
         # Export redacted image
         output_path = export_redacted_image(image, redacted_cords)
+
+        return output_path, agents_speech
+    
+    
+class PDFRedactionService:
+    def __init__(self, degree=0, guardrail_toggle=1):
+        self.degree = degree
+        self.guardrail_toggle = 0
+        # Temporary: Toggling guardrails on for 3rd degree only
+        if degree == 2 and guardrail_toggle:
+            self.guardrail_toggle = 1
+
+        self.assistant = PDFRedactionAgents(degree).assistant
+        self.degree0_list = PDFRedactionAgents(degree).degree0_list
+        self.degree1_list = PDFRedactionAgents(degree).degree1_list
+        self.degree2_list = PDFRedactionAgents(degree).degree2_list
+    
+    def redact_pdf(self, pdf):
+        result = azure_pdf_ocr(pdf)
+        redacted_list = []
+        redacted_list_from_agent = []
+
+        for page in result.pages:
+            raw_redacted_list_from_agent_page = self.assistant(" ".join([line.content for line in page.lines]), aggregation_strategy="first")
+            for entity in raw_redacted_list_from_agent_page:
+                if self.degree == 0:
+                    if entity['entity_group'] in self.degree0_list:
+                        redacted_list_from_agent += entity['word'].strip().split()
+                elif self.degree == 1:
+                    if entity['entity_group'] in self.degree0_list + self.degree1_list:
+                        redacted_list_from_agent += entity['word'].strip().split()
+                elif self.degree == 2:
+                    if entity['entity_group'] in self.degree0_list + self.degree1_list + self.degree2_list:
+                        redacted_list_from_agent += entity['word'].strip().split()
+
+        redacted_list_from_agent = list(set(redacted_list_from_agent))
+
+        # Return chat history
+        agents_speech = []
+        agents_speech.append('<h4>' + 'assistant' + '</h4>')
+        agents_speech.append('<p>' + str(redacted_list_from_agent) + '</p>')
+        
+        # Guardrails are called only for last degree
+        if self.guardrail_toggle:
+            redacted_list_no_proper_nouns = guardrail_proper_nouns(result.content.split())
+            redacted_list = list(set(redacted_list_from_agent + redacted_list_no_proper_nouns))
+
+            agents_speech.append('<h4>' + 'guardrail: redacting proper nouns' + '</h4>')
+            agents_speech.append('<p>' + str(redacted_list_no_proper_nouns) + '</p>')
+        else:
+            redacted_list = redacted_list_from_agent
+        redacted_list = sorted(redacted_list, key=len, reverse=True)
+
+        # Extract redacted words and their coordinates
+        redacted_cords = []
+        page_dims = []
+        print(result)
+        for page in result.pages:
+            page_dims.append((page.width, page.height))
+            redacted_cords_page = []
+            for word in page.words:
+                for redacted_word in redacted_list:
+                    if redacted_word in word.content:
+                        cords = []
+                        for polygon in word.polygon:
+                            cords.append((polygon.x, polygon.y))
+                        redacted_cords_page.append(cords)
+            redacted_cords.append(redacted_cords_page)
+
+        # Export redacted PDF
+        output_path = export_redacted_pdf(pdf, redacted_cords, page_dims)
 
         return output_path, agents_speech
 

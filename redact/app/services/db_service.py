3817d42ac9d276b3d36fe1f3b55ba21c375dfb39
Uddhav Narayan Bhat <bhatuddhav@gmail.com>
2024-09-30 21:44:45 +0530

    feat: displayDB

--- redact/app/services/db_service.py
+++ redact/app/services/db_service.py
@@ -1,7 +1,14 @@
 from ..models import modelTrainingData
+import pandas as pd
+
 def uploadOutputDB(dict_struct):
-    
    # To create a new object and push it into the database do the below
    for data in dict_struct:
       modelTrainingData.objects.create(**data)
    # (**) is used to let django know to convert the dictionary key:values to be mapped onto the django model class.
+
+def displayDB():
+   response = modelTrainingData.objects.all().values()
+   df = pd.DataFrame(response)
+   print(df)
+
